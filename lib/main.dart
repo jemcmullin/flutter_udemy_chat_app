ad1fc17091cbe58d9dd19ad63b3b246956f98e0e
jemcmullin <jemcmullin@gmail.com>
2021-04-20 21:09:44 -0500

    Setup Firestore and test intial connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../screens/splash_screen.dart';
 import '../screens/chat_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  //initialize Firebase
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChatScreen(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: 'Chat App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: snapshot.connectionState != ConnectionState.done
+              ? SplashScreen()
+              : ChatScreen(),
+        );
+      },
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('Static Text for Testing'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/c2sJn4WqGyDHI69z5kaA/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((doc) {
+              print(doc['text']);
+            });
+          });
+        },
       ),
     );
   }
