ea9c44da161a6a6e1744baa9c747374495bbcbda
jemcmullin <jemcmullin@gmail.com>
2021-04-26 20:00:24 -0500

    add Stream handling of home route based on Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../screens/splash_screen.dart';
 import '../screens/chat_screen.dart';
 import '../screens/auth_screen.dart';
@@ -31,7 +32,15 @@ class MyApp extends StatelessWidget {
           ),
           home: snapshot.connectionState != ConnectionState.done
               ? SplashScreen()
-              : AuthScreen(),
+              : StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ChatScreen();
+                    }
+                    return AuthScreen();
+                  },
+                ),
         );
       },
     );
